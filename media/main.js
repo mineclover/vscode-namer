6bd7b7f1ebe9890b56985c7ac538c5805d7541e5
bangjunwoo <mineclover@naver.com>
2024-07-23 08:12:52 +0900

    feat: simple is best

--- media/main.js
+++ media/main.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-undef */
-
 // main.js
 (function () {
   const vscode = acquireVsCodeApi();
   const suggestButton = document.getElementById("suggest");
-  const refineButton = document.getElementById("refine");
   const input = document.getElementById("input");
   const suggestionsDiv = document.getElementById("suggestions");
-  const refinedSuggestionsDiv = document.getElementById("refinedSuggestions");
   const namingStyleSelect = document.getElementById("namingStyle");
 
   suggestButton.addEventListener("click", () => {
@@ -20,44 +17,21 @@
     });
   });
 
-  refineButton.addEventListener("click", () => {
-    const text = input.value;
-    const style = namingStyleSelect.value;
-    const selectedSuggestions = getSelectedSuggestions();
-    vscode.postMessage({
-      type: "refine",
-      value: text,
-      style: style,
-      selectedSuggestions: selectedSuggestions,
-    });
-  });
-
   window.addEventListener("message", (event) => {
     const message = event.data;
     switch (message.type) {
       case "suggestions":
-        displaySuggestions(message.value, suggestionsDiv);
-        break;
-      case "refinedSuggestions":
-        displaySuggestions(message.value, refinedSuggestionsDiv);
+        displaySuggestions(message.value);
         break;
     }
   });
 
-  function displaySuggestions(suggestions, targetDiv) {
-    targetDiv.innerHTML = "";
+  function displaySuggestions(suggestions) {
+    suggestionsDiv.innerHTML = "";
     suggestions.forEach((suggestion, index) => {
       const div = document.createElement("div");
       div.className = "suggestion";
-
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.id = `suggestion-${targetDiv.id}-${index}`;
-      checkbox.value = suggestion;
-
-      const label = document.createElement("label");
-      label.htmlFor = `suggestion-${targetDiv.id}-${index}`;
-      label.textContent = suggestion;
+      div.textContent = suggestion;
 
       const copyButton = document.createElement("button");
       copyButton.textContent = "Copy";
@@ -68,17 +42,8 @@
         });
       });
 
-      div.appendChild(checkbox);
-      div.appendChild(label);
       div.appendChild(copyButton);
-      targetDiv.appendChild(div);
+      suggestionsDiv.appendChild(div);
     });
-  }
-
-  function getSelectedSuggestions() {
-    const allCheckboxes = document.querySelectorAll(
-      'input[type="checkbox"]:checked'
-    );
-    return Array.from(allCheckboxes).map((checkbox) => checkbox.value);
   }
 })();
